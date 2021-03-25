f1e5dadb12506c08695eae98c8977ac9ac79b02c
Ubuntu <ubuntu@ip-172-31-29-2.us-east-2.compute.internal>
2021-03-25 09:55:23 +0000

    added new tracker file for ball possession and new config files

--- src/configs/inference_baseline_ball.py
+++ src/configs/inference_baseline_ball.py
@@ -1,5 +1,6 @@
 custom_val = True
-load_model = '../exp/mot/custom_5vals_colors_all_30ep_repr/model_30.pth'
+load_model = '../exp/mot/custom_5vals_colors_all_30ep_ball/model_30.pth'
+ball_weight = 1
 conf_thres = 0.4
 input_video = '/home/ubuntu/oljike/data/videos1/demo_vids/1.mp4'
 output_root = '../demos/custom_5vals_colors_all_30ep_debug'
