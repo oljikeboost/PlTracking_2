f0cfc5f374b983750ee760fe5470b1b639482b72
Ubuntu <ubuntu@ip-172-31-29-2.us-east-2.compute.internal>
2021-03-25 03:41:15 +0000

    removed old name config files

--- src/configs/baseline_conf_ball.py
+++
@@ -1,7 +0,0 @@
-exp_id = 'custom_5vals_colors_all_30ep_ball'
-load_model = '../pretrained/fairmot_dla34.pth'
-num_epochs = 30
-lr_step = '15'
-data_cfg = '../src/lib/cfg/custom.json'
-color_weight = 1.05
-ball_weight = 1.05
--- src/configs/baseline_conf_repr2.py
+++
@@ -1,7 +0,0 @@
-exp_id = 'custom_5vals_colors_all_30ep_repr2'
-load_model = '../pretrained/fairmot_dla34.pth'
-num_epochs = 10
-lr_step = '15'
-data_cfg = './lib/cfg/custom.json'
-color_weight = 1.05
-ball_weight = 0.0
