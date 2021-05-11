0ff7c5523aa2be22378523b09c3b7fb5e34118f7
Ubuntu <ubuntu@ip-172-31-29-2.us-east-2.compute.internal>
2021-05-11 11:27:53 +0000

    added recheck import

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -9,6 +9,7 @@ import os
 
 from .networks.dlav0 import get_pose_net as get_dlav0
 from .networks.pose_dla_dcn import get_pose_net as get_dla_dcn
+from .networks.pose_dla_dcn_recheck import get_pose_net as get_dla_dcn_recheck
 from .networks.pose_dla_dcn_ccn import get_pose_net as get_dla_dcn_ccn
 from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
 from .networks.resnet_fpn_dcn import get_pose_net as get_pose_net_fpn_dcn
@@ -18,6 +19,7 @@ from .networks.pose_dla_conv import get_pose_net as get_dla_conv
 _model_factory = {
     'dlav0': get_dlav0,  # default DLAup
     'dla': get_dla_dcn,
+    'dlarecheck': get_dla_dcn_recheck,
     'dlaccn': get_dla_dcn_ccn,
     'dlaconv': get_dla_conv,
     'resdcn': get_pose_net_dcn,
